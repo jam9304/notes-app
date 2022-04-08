a47693c11702a8a2e048aaa4ac4f75fddf2192c4
James Mooney <jmooney9304@gmail.com>
2022-04-08 19:16:13 +0100

    mockImplementation to mock createNotes
    Enables view.test.js to pass with no fetches
    
    Co-authored-by: Abdul Rehman Hussain <arhussain1@users.noreply.github.com>

--- notesView.test.js
+++ notesView.test.js
@@ -9,6 +9,8 @@ const NotesApi = require('./notesApi');
 
 require('jest-fetch-mock').enableMocks();
 
+jest.mock('./notesApi');
+
 
 describe('Page view', () => {
   it('displays 2 divs, 1 div for each note', () => {
@@ -25,64 +27,34 @@ describe('Page view', () => {
   it('Shows notes after we click button', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
     const model = new NotesModel();
-    const api = new NotesApi();
-    const view = new NotesView(model, api);
+    const mockApi = new NotesApi();
+    const view = new NotesView(model, mockApi);
     
     const addButtonEl = document.querySelector('#submit');
     const inputEl = document.querySelector('#message-input');
-    
-    fetch.mockResponseOnce(async (request) => {
-      
-      console.log(`Hello this is a request`)
-      console.log(request.method)
 
-      try {
-        expect(request.method).toBe('POST');
-      } catch (error) {
-        done(error);
-      }
-
-      return JSON.stringify({
-        0: "Get Milk"
-      });
-      
-    });
+    mockApi.createNote.mockImplementation(() => {})
 
     inputEl.value = 'Hey';
     addButtonEl.click();
-    // inputEl.value = 'Hello';
-    // addButtonEl.click()
+    inputEl.value = 'Hello';
+    addButtonEl.click()
 
     expect(document.querySelector('.note')).not.toBeNull();
     expect(document.querySelectorAll('.note')[0].innerText).toBe('Hey');
-    // expect(document.querySelectorAll('.note')[1].innerText).toBe('Hello');
+    expect(document.querySelectorAll('.note')[1].innerText).toBe('Hello');
   });
 
   it ('clears the text box after note has been added', () => {
     document.body.innerHTML = fs.readFileSync('./index.html');
     const model = new NotesModel();
-    const api = new NotesApi();
-    const view = new NotesView(model, api);
+    const mockApi = new NotesApi();
+    const view = new NotesView(model, mockApi);
 
     const addButtonEl = document.querySelector('#submit');
     const inputEl = document.querySelector('#message-input');
     
-    fetch.mockResponseOnce(async (request) => {
-
-      console.log(`Hello this is a request`)
-      console.log(request.method)
-
-      try {
-        expect(request.method).toBe('POST');
-      } catch (error) {
-        done(error);
-      }
-
-      return JSON.stringify({
-        0: "Get Milk"
-      });
-      
-    });
+    mockApi.createNote.mockImplementation(() => {})
 
     inputEl.value = 'Hey';
     addButtonEl.click();
